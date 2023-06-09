eb2e2656db7bc8305e5efb0084a2e400181dca62
ChrisChrome <christophercookman@gmail.com>
2023-06-09 17:55:41 -0600

    First test, gotta go on git so i can test on the server

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const config = require("./config.json")
+// setup function to use child process to run steamcmd
+const { exec } = require("child_process");
+
+const filter = (stdout) => {
+	// Used to filter stdout of the following steamcmd command
+	// steamcmd +force_install_dir ${config.install_dir} +login anonymous +app_info_status 1 +app_status ${config.app_id} +quit
+	// Split the stdout by new line
+	const lines = stdout.split("\n");
+	// Filter the lines for the line containing "BuildID"
+	const buildIDLine = lines.filter(line => line.includes("BuildID"))[0];
+	// split by BuildID
+	const buildID = buildIDLine.split("BuildID")[1].trim();
+	// return the buildID
+	return buildID;
+}
+
+const checkForUpdate = async () => {
+	await axios.get(`https://api.steamcmd.net/v1/info/${config.app_id}`).then(async (response) => {
+		await exec(`steamcmd +force_install_dir ${config.install_dir} +login anonymous +app_info_update 1 +app_info_print ${config.app_id} +quit`, (error, stdout, stderr) => {
+			if (error) {
+				console.log(`error: ${error.message}`);
+				return;
+			}
+			if (stderr) {
+				console.log(`stderr: ${stderr}`);
+				return;
+			}
+			const buildID = filter(stdout);
+			if (buildID !== response.data.data.depots.branches[config.branch].buildid) {
+				console.log("Update Available");
+				// download update
+				// would update here
+			} else {
+				console.log("No Update Available");
+			}
+		});
+	})
+}
+
+checkForUpdate();
