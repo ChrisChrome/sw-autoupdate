40f838f4184b20d46a54b8d3f0a5091b941ce2b9
ChrisChrome <christophercookman@gmail.com>
2023-06-09 18:40:10 -0600

    add interval

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const config = require("./config.json")
 const { exec } = require("child_process");
 
 const filter = (stdout) => {
+	console.log(stdout)
 	// Used to filter stdout of the following steamcmd command
 	// steamcmd +force_install_dir ${config.install_dir} +login anonymous +app_info_status 1 +app_status ${config.app_id} +quit
 	// Split the stdout by new line
@@ -17,7 +18,34 @@ const filter = (stdout) => {
 	return buildID;
 }
 
+const updateServer = async () => {
+	exec(`steamcmd +force_install_dir ${config.install_dir} +login anonymous +app_update ${config.app_id} validate +quit`, (error, stdout, stderr) => {
+		if (error | stderr) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		console.log(stdout);
+		exec(`steamcmd +force_install_dir ${config.install_dir} +login anonymous +app_update ${config.app_id} validate +quit`, (error, stdout, stderr) => { // Verify the update
+			if (error | stderr) {
+				console.log(`error: ${error.message}`);
+				return;
+			}
+			console.log(stdout);
+			if(config.after_update_command) {
+				exec(config.after_update_command, (error, stdout, stderr) => { // Run the after update command
+					if (error | stderr) {
+						console.log(`error: ${error.message}`);
+						return;
+					}
+					console.log(stdout);
+				});
+			}
+		});
+	});
+}
+
 const checkForUpdate = async () => {
+	console.log("Checking for update...")
 	await axios.get(`https://api.steamcmd.net/v1/info/${config.app_id}`).then(async (response) => {
 		await exec(`steamcmd +force_install_dir ${config.install_dir} +login anonymous +app_info_update 1 +app_status ${config.app_id} +quit`, (error, stdout, stderr) => {
 			if (error) {
@@ -32,29 +60,7 @@ const checkForUpdate = async () => {
 			console.log(response.data.data[config.app_id].depots.branches[config.check_branch].buildid)
 			if (buildID !== response.data.data[config.app_id].depots.branches[config.check_branch].buildid || config.force_update) {
 				console.log("!!! Update Available !!!");
-				exec(`steamcmd +force_install_dir ${config.install_dir} +login anonymous +app_update ${config.app_id} validate +quit`, (error, stdout, stderr) => {
-					if (error | stderr) {
-						console.log(`error: ${error.message}`);
-						return;
-					}
-					console.log(stdout);
-					exec(`steamcmd +force_install_dir ${config.install_dir} +login anonymous +app_update ${config.app_id} validate +quit`, (error, stdout, stderr) => { // Verify the update
-						if (error | stderr) {
-							console.log(`error: ${error.message}`);
-							return;
-						}
-						console.log(stdout);
-						if(config.after_update_command) {
-							exec(config.after_update_command, (error, stdout, stderr) => { // Run the after update command
-								if (error | stderr) {
-									console.log(`error: ${error.message}`);
-									return;
-								}
-								console.log(stdout);
-							});
-						}
-					});
-				});
+				updateServer();
 					
 			} else {
 				console.log("No Update Available, continuing...");
@@ -64,3 +70,7 @@ const checkForUpdate = async () => {
 }
 
 checkForUpdate();
+
+setInterval(() => {
+	checkForUpdate();
+}, config.check_interval_mins * 60000)
