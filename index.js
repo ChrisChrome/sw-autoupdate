a8a41ae706bf3316a662688a97b3f85bbda01275
ChrisChrome <christophercookman@gmail.com>
2023-06-09 18:01:41 -0600

    test

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const fs = require("fs");
+const axios = require("axios");
 const config = require("./config.json")
 // setup function to use child process to run steamcmd
 const { exec } = require("child_process");
 
 const filter = (stdout) => {
+	console.log(stdout)
 	// Used to filter stdout of the following steamcmd command
 	// steamcmd +force_install_dir ${config.install_dir} +login anonymous +app_info_status 1 +app_status ${config.app_id} +quit
 	// Split the stdout by new line
 	const lines = stdout.split("\n");
+	console.log(lines)
 	// Filter the lines for the line containing "BuildID"
 	const buildIDLine = lines.filter(line => line.includes("BuildID"))[0];
+	console.log(buildIDLine)
 	// split by BuildID
 	const buildID = buildIDLine.split("BuildID")[1].trim();
 	// return the buildID
@@ -18,7 +22,7 @@ const filter = (stdout) => {
 
 const checkForUpdate = async () => {
 	await axios.get(`https://api.steamcmd.net/v1/info/${config.app_id}`).then(async (response) => {
-		await exec(`steamcmd +force_install_dir ${config.install_dir} +login anonymous +app_info_update 1 +app_info_print ${config.app_id} +quit`, (error, stdout, stderr) => {
+		await exec(`steamcmd +force_install_dir ${config.install_dir} +login anonymous +app_info_update 1 +app_status ${config.app_id} +quit`, (error, stdout, stderr) => {
 			if (error) {
 				console.log(`error: ${error.message}`);
 				return;
